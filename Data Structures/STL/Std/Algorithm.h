62f77e4c5d597935aebde32fff309c497d7ece2d
Yungkee <lev-118@mail.ru>
2018-07-15 16:45:55 +0300

    implementation algorithm Search

--- Data Structures/STL/Std/Algorithm.h
+++ Data Structures/STL/Std/Algorithm.h
@@ -43,4 +43,88 @@ namespace STL
 		}
 	}
 
+#pragma region Search
+
+	template<typename T>
+	RandomAccessIterator<T> Find(RandomAccessIterator<T> pBegin, RandomAccessIterator<T> pEnd, const T& value)
+	{
+		while (pBegin != pEnd)
+		{
+			if (*pBegin == value)
+				return pBegin;
+			++pBegin;
+		}
+		return pEnd;
+	}
+
+	template<typename T>
+	RandomAccessConstIterator<T> Find(RandomAccessConstIterator<T> pBegin, RandomAccessConstIterator<T> pEnd, const T& value)
+	{
+		while (pBegin != pEnd)
+		{
+			if (*pBegin == value)
+				return pBegin;
+			++pBegin;
+		}
+		return pEnd;
+	}
+
+	template<typename T, typename UnaryPredicate>
+	RandomAccessIterator<T> FindIf(RandomAccessIterator<T> pBegin, RandomAccessIterator<T> pEnd, UnaryPredicate functor)
+	{
+		while (pBegin != pEnd)
+		{
+			if (functor(*pBegin))
+				return pBegin;
+			++pBegin;
+		}
+		return pEnd;
+	}
+
+	template<typename T, typename UnaryPredicate>
+	RandomAccessConstIterator<T> FindIf(RandomAccessConstIterator<T> pBegin, RandomAccessConstIterator<T> pEnd, UnaryPredicate functor)
+	{
+		while (pBegin != pEnd)
+		{
+			if (functor(*pBegin))
+				return pBegin;
+			++pBegin;
+		}
+		return pEnd;
+	}
+
+#pragma region Search
+
+	template<class T>
+	void Reverse(RandomAccessIterator<T> pBegin, RandomAccessIterator<T> pEnd)
+	{
+		while (pBegin != pEnd && pBegin != --pEnd)
+		{
+			Swap(*pBegin, *pEnd);
+			++pBegin;
+		}
+	}
+
+	template<class T>
+	void Replace(RandomAccessIterator<T> pBegin, RandomAccessIterator<T> pEnd, const T& oldValue, const T& newValue)
+	{
+		while (pBegin != pEnd)
+		{
+			if (*pBegin == oldValue)
+				*pBegin = newValue;
+			++pBegin;
+		}
+	}
+
+	template<class T, typename UnaryPredicate>
+	void ReplaceIf(RandomAccessIterator<T> pBegin, RandomAccessIterator<T> pEnd, UnaryPredicate predicate, const T& newValue)
+	{
+		while (pBegin != pEnd)
+		{
+			if (predicate(*pBegin))
+				*pBegin = newValue;
+			++pBegin;
+		}
+	}
+
 }
